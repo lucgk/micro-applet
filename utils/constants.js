6e7db81e331cbbe1ec780be4aa430c0ebc4ef850
wood <391343732@qq.com>
2019-03-16 15:20:32 +0800

    factory

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+//url相关
+var GET_URL = BASE_URL.concat("/history/content/100/1");
+
+//error相关
+var ERROR_DATA_IS_NULL = "获取数据为空，请重试";
+
+//各个page的URL
+var FACTORY_DETAIL = "/pages/factoryInfo/factoryDetail/factoryDetail";
+
+
+module.exports = {
+  GET_URL: GET_URL,
+  ERROR_DATA_IS_NULL: ERROR_DATA_IS_NULL,
+  FACTORY_DETAIL: FACTORY_DETAIL
+}
