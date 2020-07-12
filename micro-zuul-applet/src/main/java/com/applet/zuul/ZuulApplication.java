dcad11ccf341068967b0702e5789140dc5f501dc
liuhui <liuhui@xiaogj.com>
2020-07-12 16:03:17 +0800

    project init

---
+++ micro-zuul-applet/src/main/java/com/applet/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.applet.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO 添加类的描述
+ *
+ * @author liuhui
+ * @version 1.0, 2020/7/12 12:06
+ */
+
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
